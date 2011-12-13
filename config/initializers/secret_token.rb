fb2074d353f4bafb58650977221fa50fde753842
EshaMukherjee <vinsol@EshaMukherjee-MacBook-Pro.local>
2011-12-13 11:23:41 +0530

    first test commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EshaYammer::Application.config.secret_token = '89f3359f0c5650a0563942ba34790f76e7f5fcd430d54082fc4f9c61252f5f379ec51546232aa1b773757f452a0bbedc02afcfd71cf6902dd90cfb1ac16e96dd'
